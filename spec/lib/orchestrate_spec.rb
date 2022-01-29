4f0d604c55cef23c8caeb327f5f99893bc242e64
Shayon Mukherjee <shayonj@gmail.com>
2022-01-29 15:37:01 -0500

    Ensure FKs from other tables are updated on swap
    Starting with a failing spec (partially

--- spec/lib/orchestrate_spec.rb
+++ spec/lib/orchestrate_spec.rb
@@ -1125,7 +1125,14 @@ RSpec.describe PgOnlineSchemaChange::Orchestrate do
                              "CREATE UNIQUE INDEX pgosc_shadow_table_for_books_email_key ON books USING btree (email)"])
     end
 
-    skip "sucessfully renames the tables and transfers foreign keys" do
+    it "sucessfully renames the tables and transfers foreign keys" do
+      described_class.swap!
+
+      foreign_keys = PgOnlineSchemaChange::Query.get_foreign_keys_for(client, "chapters")
+      expect(foreign_keys).to eq([
+                                { "table_on" => "chapters", "table_from" => "books",
+                                  "constraint_type" => "f", "constraint_name" => "chapters_book_id_fkey", "definition" => "FOREIGN KEY (book_id) REFERENCES books(user_id" },
+                              ])
     end
   end
 end
