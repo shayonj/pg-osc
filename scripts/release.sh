180177ef3d98f58f9ed554a320a04a56d9186410
Shayon Mukherjee <shayonj@gmail.com>
2023-06-24 12:14:49 -0400

    Update CI to push docker images from CI

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,28 +1,22 @@
+#!/bin/bash
+
+set -euo pipefail
+
 export VERSION=$1
 echo "VERSION: ${VERSION}"
-
-echo "=== Pushing tags to github ===="
-git tag v$VERSION
-git push origin --tags
 
 echo "=== Building Gem ===="
 gem build pg_online_schema_change.gemspec
 
 echo "=== Pushing gem ===="
-gem push pg_online_schema_change-$VERSION.gem
+gem push pg_online_schema_change-"$VERSION".gem
 
-echo "=== Sleeping for 5s ===="
-sleep 5 
+echo "=== Sleeping for 15s ===="
+sleep 15
 
-echo "=== Building Image ===="
-docker build . --build-arg VERSION=$VERSION -t shayonj/pg-osc:$VERSION
-
-echo "=== Tagging Image ===="
-docker image tag shayonj/pg-osc:$VERSION shayonj/pg-osc:latest
-
-echo "=== Pushing Image ===="
-docker push shayonj/pg-osc:$VERSION
-docker push shayonj/pg-osc:latest
+echo "=== Pushing tags to github ===="
+git tag v"$VERSION"
+git push origin --tags
 
 echo "=== Cleaning up ===="
-rm pg_online_schema_change-$VERSION.gem
+rm pg_online_schema_change-"$VERSION".gem
